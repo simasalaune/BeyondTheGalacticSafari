abad772487aa47a3aeaabdd7cdd6276da3eabdb7
JustelisV <125958264+JustelisV@users.noreply.github.com>
2023-03-30 19:11:42 +0300

    added SkinRoom

---
+++ app/src/main/java/com/example/beyondthegalacticsafari/SkinRoom.java
@@ -0,0 +1,57 @@
+package com.example.beyondthegalacticsafari;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SkinRoom extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_skin);
+        Button nextButton = findViewById(R.id.next_button);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeSkin();
+            }
+        });}
+
+    private int skinNumber = 1;
+
+    private void changeSkin() {
+        skinNumber++;
+        if (skinNumber > 3) {
+            skinNumber = 1;
+        }
+        Button nextButton = findViewById(R.id.next_button);
+        ImageView skin = findViewById(R.id.skins);
+        switch (skinNumber) {
+            case 1:
+                nextButton.setText("Next");
+                nextButton.setCompoundDrawablesWithIntrinsicBounds(R.drawable.skin, 0, 0, 0);
+                skin.setImageResource(R.drawable.main_ship___base___full_health);
+                // TODO:
+                break;
+            case 2:
+                nextButton.setText("Next");
+                nextButton.setCompoundDrawablesWithIntrinsicBounds(R.drawable.skin, 0, 0, 0);
+                skin.setImageResource(R.drawable.emissary);
+                // TODO:
+                break;
+            case 3:
+                nextButton.setText("Next");
+                nextButton.setCompoundDrawablesWithIntrinsicBounds(R.drawable.skin, 0, 0, 0);
+                skin.setImageResource(R.drawable.beholder);
+                // TODO:
+                break;
+        }
+    }
+    public void back(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
