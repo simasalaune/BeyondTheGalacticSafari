31f1c21a6874cf7139264cdd02be02f72b5e9299
Egle <103460996+IdiotAllex@users.noreply.github.com>
2023-04-08 10:10:31 +0300

    shipFix

--- app/src/main/java/com/example/beyondthegalacticsafari/Ship.java
+++ app/src/main/java/com/example/beyondthegalacticsafari/Ship.java
@@ -16,7 +16,7 @@ public class Ship {
 
     Ship(int screenX, int screenY, Resources resources) {
         if (ShipBitmap.ship == null) {
-            ship = BitmapFactory.decodeResource(resources, R.drawable.main_ship___base___full_health);
+            ship = BitmapFactory.decodeResource(resources, R.drawable.shipthree);
         } else {
             ship = ShipBitmap.ship;
         }
@@ -25,8 +25,8 @@ public class Ship {
         height = ship.getHeight();
 
 
-        width *= 2;
-        height *= 2;
+        width /= 24;
+        height /= 24;
 
         if ((int) screenRatioX != 0) {
             width /= (int) screenRatioX;
